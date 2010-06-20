d558642c16018e522da58436d1cc7c0a69fdc379
steph <urschrei@gmail.com>
2010-06-20 03:03:32 +0100

    first commit

---
+++ global.js
@@ -0,0 +1,20 @@
+//wait for messages from expunge.js
+safari.application.addEventListener("message", respondToMessage, false);
+
+// getter and setter function for settings used by expunge.js
+function respondToMessage(messageEvent) {
+    if (messageEvent.name == "getSettingValue") {
+        // getItem("foo");
+        var value = safari.extension.settings.getItem(messageEvent.message);
+        // return value of foo to expunge.js
+        safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("settingValueIs", value);
+    }
+    else if (messageEvent.name == "setSettingValue") {
+	var tmp = messageEvent.message;
+	tmp = tmp.split('?'); //using ? as a delimiter
+	safari.extension.settings.setItem(tmp[0], tmp[1]);
+    }
+    else return; //unknown message, do nothing
+}
+
+
