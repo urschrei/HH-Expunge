db3880e3c69c80926002a5a88ce25fe0e7bd226b
Stephan Hügel <urschrei@gmail.com>
2010-12-09 02:00:57 +0000

    Implementing: a (basic) options page, localStorage of users retrieval of users via callbacks

--- chrome/chrome_script.js
+++ chrome/chrome_script.js
@@ -3,55 +3,12 @@ String.prototype.trim = function() {
 	return this.replace(/^\s+|\s+$/g, "");
 };
 
-chrome.extension.onRequest.addListener("message", getMessage, false);
-// event listener for incoming requested messages from background.html
-chrome.extension.sendRequest("getSettingValue", "blacklist");
-// ask for value from global.js, since only it can interact with the Safari object directly
-function getMessage(msgEvent) {
-	if (msgEvent.name == "settingValueIs") retrieved = msgEvent.message;
-	storeBlacklist(retrieved);
-}
-
-
-
-function storeBlacklist(bl) {
-	if ("user1,user2,user3" != bl) {
-		console.log("Writing user-defined blacklist values to local storage");
-		console.log("Values: " + bl);
-		bl = bl.trim();
-		//strip leading and trailing whitespace
-		localStorage.clear();
-		//clear all local storage, since we're currently only using one field
-		try {
-			localStorage.setItem("ignorelist", bl);
-		} catch(e) {
-			console.log("Couldn't write local storage item. I think this\
-			is a bug, and am proceeding with ignored-user comment removal anyway.");
-			localStorage.clear();
-			kill(bl);
-		}
-
-		kill(bl);
-	} else
-	// default values present: extension is in use for the first time, or post-update
-	if (localStorage.getItem("ignorelist")) {
-		//stored values exist, so use those, and restore them to the user prefs
-		console.log("Retrieving local storage blacklist values: ");
-		kl = localStorage.getItem("ignorelist");
-		kl = kl.trim();
-		//strip leading and trailing whitespace
-		console.log("Retrieved values: " + kl + " … restoring your preferences");
-		safari.self.tab.dispatchMessage("setSettingValue", "blacklist?" + kl);
-		//using ? to delimit in order to avoid splitting the values
-		kill(kl);
-	} else
-	//no stored values were found, and the default is in use, so alert the user
-	alert("You haven't defined any users to ignore, please go to the HumHum\
-	User Ignore extension preferences,\
-	and add some (use lowercase for user names).");
-
-}
-
+chrome.extension.sendRequest({localstorage: "expunge_users"}, function(response) {
+    console.log("Asking for users…");
+    var retrieved = response.expunge_users;
+    console.log("Expunging users: " + retrieved);
+	kill(retrieved);
+});
 
 
 function kill(users) {
